789fb9e607193f9e6ba7e120f1d17a6c8dc51b09
abhishekanalytics <abhishekanalytics2023@gmail.com>
2024-02-16 17:53:22 +0530

    create password_property

--- Flask_app/config.py
+++ Flask_app/config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 
 class Config:
+
     DATABASE_URI = os.getenv('DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
 
--- Flask_app/models/models.py
+++ Flask_app/models/models.py
@@ -1,5 +1,11 @@
 from sqlalchemy import Column , Integer , String 
 from Flask_app import db
+from sqlalchemy.ext.hybrid import hybrid_property
+from werkzeug.security import(
+    generate_password_hash,
+    check_password_hash
+    )
+
 
 class User(db.Model):
 
@@ -12,5 +18,26 @@ class User(db.Model):
     firstname=Column(String(20),nullable=False)
     lastname=Column(String(20),nullable=False)
     mobile_number=Column(String(20),nullable=False)
-    h_password = Column('password', String(350), nullable=False)
+    hash_password = Column('password', String(350), nullable=False)
     age = db.Column(db.Integer)
+    
+    @hybrid_property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, user_password):
+        self._password = generate_password_hash(user_password)
+
+    def verify_password(self, user_password):
+        return check_password_hash(self.hash_password, user_password)
+
+    def __repr__(self) -> str:
+        return '<User %r>' % self.email
+
+
+
+
+
+
+
