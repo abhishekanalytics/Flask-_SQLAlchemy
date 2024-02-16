400cb1fb3543111b7a25820ab614a42e65feb014
abhishekanalytics <abhishekanalytics2023@gmail.com>
2024-02-16 13:39:17 +0530

    create user model

--- Flask_app/config.py
+++ Flask_app/config.py
@@ -1,9 +1,12 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 
 class Config:
     DATABASE_URI = os.getenv('DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
 
+@staticmethod
+def init_app(app):
+    pass
