870cd724d02323737f1d8053fb74f5be00e65647
abhishekanalytics <abhishekanalytics2023@gmail.com>
2024-02-16 13:06:48 +0530

    add user model in models dir

--- Flask_app/views/user.py
+++ Flask_app/views/user.py
@@ -1,5 +1,7 @@
 from flask import jsonify,Blueprint
+
 admin = Blueprint('admin', __name__, url_prefix='/admin')
+
 @admin.route('/')
 def index():
     return jsonify({"msg":"This is basic application factory"})
